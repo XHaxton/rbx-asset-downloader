580a1172d7e71bb7d5b5929abfcc56077f24cd41
SkylarZYX <dashystudios2206@gmail.com>
2024-07-21 21:00:44 -0400

    batch edit

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@ cd rbx-plugin
 move AssetDownloader.lua %localappdata%\Roblox\Plugins
 cd %~dp0
 rmdir rbx-plugin
-echo COPY/PASTE URLS HERE, AND DELETE THIS TEXT! > astUrls.txt
+echo copy/paste URLs here (DELETE THIS TEXT!) > astUrls.txt
 echo Enter API key here (DELETE THIS TEXT) > key.txt
 echo installed successfully!
